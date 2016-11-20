f7d796809730411a26ff459e570ab55e502517ed
SAM Solutions Laptop <showrav017@gmail.com>
2016-11-20 18:11:44 +0600

    fixing issues

---
+++ single-board_of_director.php
@@ -0,0 +1,41 @@
+<?php get_header() ?>
+    <!-- Page Head -->
+    <!-- Content -->
+    <div  class="ver_height_4"></div>
+    <div class="container contents lisitng-grid-layout">
+
+        <div class="row">
+
+            <div class="span12 main-wrap">
+
+                <!-- Main Content -->
+                <div class="main">
+
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                        <section class="listing-layout">
+                            <h3 class="title-heading"><?php the_title() ?></h3>
+
+                            <?php the_post_thumbnail("full"); ?>
+
+                            <div class="list-container">
+                                <?php the_content(); ?>
+                            </div>
+
+                        </section>
+
+                        <?php
+                    endwhile; else: ?>
+                        <p>Sorry, no posts matched your criteria.</p>
+                    <?php endif; ?>
+
+
+
+
+                </div><!-- End Main Content -->
+
+            </div> <!-- End span9 -->
+        </div><!-- End contents row -->
+
+    </div><!-- End Content -->
+<?php get_footer(); ?>
